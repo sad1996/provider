27af3d740aa7b1257acd6c54fe1d8239dff38338
Mohamed Sadakathulla <sadakkathullah@gmail.com>
2019-05-23 20:14:02 +0530

    Update provider.dart

--- lib/src/provider.dart
+++ lib/src/provider.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_web/foundation.dart';
+import 'package:flutter_web/widgets.dart';
 import 'package:provider/src/adaptive_builder_widget.dart';
 
 /// A function that disposes of [value].
