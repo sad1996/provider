825677567100be634c73742b67fbb482ffc9cd4f
Mohamed Sadakathulla <sadakkathullah@gmail.com>
2019-05-23 20:13:43 +0530

    Update listenable_provider.dart

--- lib/src/listenable_provider.dart
+++ lib/src/listenable_provider.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_web/foundation.dart';
+import 'package:flutter_web/material.dart';
 import 'package:provider/src/adaptive_builder_widget.dart';
 import 'package:provider/src/provider.dart';
 
